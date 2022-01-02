23635bf1a0454b82d39c8bddb1193bab5ffd2324
Samuel Kupka <skupka@sageteam.eu>
2022-01-02 21:20:31 +0100

    Bump libfmt to 8.1.0

--- include/shaga/common.h
+++ include/shaga/common.h
@@ -1,7 +1,7 @@
 /******************************************************************************
 Shaga library is released under the New BSD license (see LICENSE.md):
 
-Copyright (c) 2012-2021, SAGE team s.r.o., Samuel Kupka
+Copyright (c) 2012-2022, SAGE team s.r.o., Samuel Kupka
 
 All rights reserved.
 *******************************************************************************/
